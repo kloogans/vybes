5c52375adcb369b1ab48fc9f4ebce1a42da1617d
James O'Brien <jtwebco@gmail.com>
2017-05-16 09:20:22 -0400

    Removes old code

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react'
-import { NavLink } from 'react-router-dom'
 
 class Logo extends Component {
   render () {
     return <div className='logo-container'>
-      <NavLink to='/'>
+      <a href='/'>
         <div className='circles'>
           <div className='logo'>
             <div className='logo-dots' />
           </div>
         </div>
-      </NavLink>
+      </a>
     </div>
   }
 }
