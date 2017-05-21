6aac85c070b5c6564bd1f770956310e0296a18fb
James O'Brien <jtwebco@gmail.com>
2017-05-21 16:51:15 -0400

    User auth gone, cleans up code

--- src/App.js
+++ src/App.js
@@ -29,26 +29,26 @@ class App extends Component {
     </Router>
   }
 
-  // componentWillMount () {
-  //   // Called the first time the component is loaded
-  //   // right before the component is added to the page.
-  // }
-  //
-  // componentDidMount () {
-  //   // Called after the component has been rendered into the page.
-  // }
-  //
-  // componentWillReceiveProps (nextProps) {
-  //   // Called when the props provided to the component are changed.
-  // }
-  //
-  // componentWillUpdate (nextProps, nextState) {
-  //   // Called when the props and/or state change.
-  // }
-  //
-  // componentWillUnmount () {
-  //   // Called when the component is removed.
-  // }
+  componentWillMount () {
+    // Called the first time the component is loaded
+    // right before the component is added to the page.
+  }
+
+  componentDidMount () {
+    // Called after the component has been rendered into the page.
+  }
+
+  componentWillReceiveProps (nextProps) {
+    // Called when the props provided to the component are changed.
+  }
+
+  componentWillUpdate (nextProps, nextState) {
+    // Called when the props and/or state change.
+  }
+
+  componentWillUnmount () {
+    // Called when the component is removed.
+  }
 }
 
 export default App
