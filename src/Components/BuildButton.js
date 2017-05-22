8db21b336070791275ff220bc0ade2c0836dbc48
James O'Brien <jtwebco@gmail.com>
2017-05-22 14:48:31 -0400

    CSS cleanup and button animations

--- src/Components/BuildButton.js
+++ src/Components/BuildButton.js
@@ -6,21 +6,26 @@ import { buildPlaylist } from '../Actions/buildPlaylist'
 import { Link } from 'react-router-dom'
 
 class BuildButton extends Component {
+  state = {
+    hover: false
+  }
+
+  _hover = () => {
+    this.setState({
+      hover: !this.state.hover
+    })
+  }
   render () {
+    const bars = [0, 1, 2, 3, 4, 5, 6, 7]
     return <div className='container section spotify-iframe'>
       <Link to='/playlist'>
-        <div className='playlist-btn'
-          onClick={() => this.props.buildPlaylist(this.props.artists)}>
+        <div className='playlist-button'
+          onClick={() => this.props.buildPlaylist(this.props.artists)}
+          onMouseOver={this._hover} onMouseOut={this._hover}>
           <div className='sound-bars'>
-            <div className='bar' />
-            <div className='bar' />
-            <div className='bar' />
-            <div className='bar' />
-            <div className='bar' />
-            <div className='bar' />
-            <div className='bar' />
-            <div className='bar' />
-            <div className='bar' />
+            {bars.map((_, i) => {
+              return <div className={this.state.hover ? 'bar move' : 'bar'} key={i} />
+            })}
           </div>
         </div>
       </Link>
