af40ba7bc92da509fb363b0cbcf2156e7d828dcf
James O'Brien <jtwebco@gmail.com>
2017-05-24 23:40:31 -0400

    Working on responsiveness

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -5,10 +5,12 @@ class Info extends Component {
   render () {
     return <div className='Info'>
       <div className='Info__info-container'>
-        <h1>Vybes</h1>
-        <p>Vybes makes it simple to create a playlist of the
+        <div className='Info__welcome'>
+          <h1>Vybes</h1>
+          <p>Vybes makes it simple to create a playlist of the
         artists that you enjoy.</p>
-        <p>Here's how it works:</p>
+          <p>Here's how it works:</p>
+        </div>
         <ul className='Info__instructions'>
           <li>Search for an artist.</li>
           <li>Click the artist name to add them to your list.</li>
@@ -16,9 +18,11 @@ class Info extends Component {
           <li>Hit the <strong>GO</strong> button at the bottom.</li>
           <li>Enjoy your custom playlist, populated by your favorite artists.</li>
         </ul>
-        <p>Now that you're ready, let's
+        <div className='Info__try-it'>
+          <p>Now that you're ready, let's
           <Link to='/'> give it a try</Link>.
         </p>
+        </div>
       </div>
     </div>
   }
